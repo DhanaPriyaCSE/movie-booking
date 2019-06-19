cd2eeb398b2a86bce5fcf275f718daca1e6b836d
Pachaiammal K <Pachaiammal.K@mrcooper.com>
2019-06-19 11:05:23 -0500

    added repository

---
+++ repository/BookingRepository.java
@@ -0,0 +1,10 @@
+package com.project.movie_booking.repository;
+
+import com.project.movie_booking.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+}
